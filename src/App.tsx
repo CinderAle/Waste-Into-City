69a6b60c05c37dd3dfe7c60dcc3553761cceb31d
CinderAle <a.d.strukov@gmail.com>
2024-12-14 00:00:26 +0300

    feat: added auth check on every app launch

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { useEffect } from 'react';
 import { YMapComponentsProvider } from 'ymap3-components';
 
+import { checkAuth } from './api/checkAuth';
 import { API } from './constants/api';
+import { useAction } from './hooks/useAction';
+import { useTypedSelector } from './hooks/useTypedSelector';
 import Admin from './pages/Admin';
+import { UserRoles } from './types/userRoles';
 
 function App() {
+    const userRole = useTypedSelector((state) => state.user.role);
+    const { signOutUserAccount } = useAction();
+
+    useEffect(() => {
+        if (userRole !== UserRoles.Guest) {
+            checkAuth().then((isRelevant) => isRelevant || signOutUserAccount());
+        }
+    }, [userRole]);
+
     return (
         <YMapComponentsProvider apiKey={API.YMAPS_KEY} lang={API.YMAPS_LANG}>
             <Admin />
